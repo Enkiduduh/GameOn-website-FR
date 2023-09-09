e2108640438e70a94e647ab96cec54584ae22ef1
Ilyes Soumar Djouma <103592860+Enkiduduh@users.noreply.github.com>
2023-09-09 20:38:20 +0200

    Add files via upload

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,30 +15,6 @@ const form = document.getElementById("myForm");
 const modalCloseBtn = document.querySelector(".close");
 const checkBox1 = document.getElementById("checkbox1");
 
-
-
-
-////////////////////////////////////////////////////////
-//Ecouter la modification de la checkbox1
-checkBox1.addEventListener('click',function() {
-  valideCheckBox1(this);
-});
-
-//Fonction vérifier la coche de la checkbox1
-const valideCheckBox1 = function(checkbox1) {
-  let error = document.querySelector("#errorcheckbox1");
-  if (checkBox1.checked) {
-    error.innerHTML="";
-  } else {
-    error.innerHTML="Vous devez vérifier que vous acceptez les termes et conditions.";
-  }
-};
-////////////////////////////////////////////////////////
-
-
-
-
-
 ////////////////////////////////////////////////////////
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -62,11 +38,81 @@ function closeModal() {
 
 
 
-
-
 ////////////////////////////////////////////////////////
-//Ecouter la modification du prénom
-form.first.addEventListener('change',function() {
+//Envoyer le formulaire
+
+
+form.addEventListener("submit", function (e) {
+let error;
+if (!form.first.value) {
+  let error = document.querySelector("#errorfirst");
+  error.innerHTML="Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+  e.preventDefault();
+}
+if (!form.last.value) {
+  let error = document.querySelector("#errorname");
+  error.innerHTML="Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+  e.preventDefault();
+}
+if (!form.email.value) {
+  let error = document.querySelector("#erroremail");
+  error.innerHTML="Vous devez saisir un email valide. ex: xxx@xx.xx";
+  e.preventDefault();
+}
+if (!form.quantity.value) {
+  let error = document.querySelector("#errorquantity");
+  error.innerHTML="Veuillez saisir une valeur numérique entre 0 et 99.";
+  e.preventDefault();
+}
+if (!checkBox1.checked) {
+  let error = document.querySelector("#errorcheckbox1");
+  error.innerHTML="Vous devez vérifier que vous acceptez les termes et conditions.";
+  e.preventDefault();
+}
+if (!form.birthdate.value) {
+  let error = document.querySelector("#errorbirthdate");
+  error.innerHTML="Vous devez saisir une date de naissance valide.";
+  e.preventDefault();
+}
+
+
+
+});
+
+const validate = function () {
+  if (form.first.value && form.last.value && form.quantity.value && checkBox1.checked && form.birthdate.value) {
+    document.querySelector(".modal-body").innerHTML = "Merci ! Votre réservation a été reçue."
+  };
+
+};
+
+
+
+// ////////////////////////////////////////////////////////
+// //Ecouter la modification de la checkbox1
+checkBox1.addEventListener('click',function() {
+  valideCheckBox1(this);
+});
+
+//Fonction vérifier la coche de la checkbox1
+const valideCheckBox1 = function(checkbox1) {
+  let error = document.querySelector("#errorcheckbox1");
+  if (checkBox1.checked) {
+    error.innerHTML="";
+  } else {
+    error.innerHTML="Vous devez vérifier que vous acceptez les termes et conditions.";
+  }
+};
+// ////////////////////////////////////////////////////////
+
+
+
+
+
+// ////////////////////////////////////////////////////////
+// //ECOUTER L'EVENEMENT INPUT SUR LE PRENOM'
+// ////////////////////////////////////////////////////////
+form.first.addEventListener('input',function() {
   valideFirst(this);
 });
 
@@ -83,11 +129,12 @@ const valideFirst = function(inputFirst) {
     error.innerHTML="Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
   }
 };
-////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////
-//Ecouter la modification du nom
-form.last.addEventListener('change',function() {
+
+// ////////////////////////////////////////////////////////
+// //ECOUTER L'EVENEMENT INPUT SUR LE NOM
+// ////////////////////////////////////////////////////////
+form.last.addEventListener('input',function() {
   valideLast(this);
 });
 //Fonction vérifier via la RegExp que le nom est conforme
@@ -103,11 +150,12 @@ const valideLast = function(inputLast) {
     error.innerHTML="Veuillez entrer 2 caractères ou plus pour le champ du nom.";
   }
 };
-////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////
-//Ecouter la modification de l'email
-form.email.addEventListener('change',function() {
+
+// ////////////////////////////////////////////////////////
+// //ECOUTER L'EVENEMENT INPUT SUR LE MAIL
+// ////////////////////////////////////////////////////////
+form.email.addEventListener('input',function() {
   valideEmail(this);
 });
 
@@ -122,35 +170,37 @@ const valideEmail = function(inputEmail) {
     error.innerHTML="Vous devez saisir un email valide. ex: xxx@xx.xx";
   }
 };
-////////////////////////////////////////////////////////
 
 
-////////////////////////////////////////////////////////
-//Ecouter la modification de la birthdate
-form.birthdate.addEventListener('change',function() {
+
+// ////////////////////////////////////////////////////////
+// //ECOUTER L'EVENEMENT INPUT SUR LA DATE DE NAISSANCE
+// ////////////////////////////////////////////////////////
+form.birthdate.addEventListener('input',function() {
   valideBirthdate(this);
 });
 
 //Fonction vérifier via la RegExp que la birthdate est conforme
 const valideBirthdate = function(element) {
-  let birthdateRegExp = new RegExp(/^((0[1-9])|(1[\d])|(2[\d])|(3[01]))\/((0[1-9])|(1[12]))\/([\d]{4})$/, 'g');
-  let testBirthdate = birthdateRegExp.test(element)
+  let birthdateRegExp = new RegExp(/^([\d]{4})[\/\-]((0[1-9])|(1[\d])|(2[\d])|(3[01]))[\/\-]((0[1-9])|(1[12]))$/, 'g');
+  let testBirthdate = birthdateRegExp.test(element.value)
   let error = document.querySelector("#errorbirthdate")
   if (testBirthdate) {
     error.innerHTML="";
   } else {
+    console.log(form.birthdate.value);
     error.innerHTML="Vous devez saisir une date de naissance valide.";
   }
 };
 ////////////////////////////////////////////////////////
 
 
-////////////////////////////////////////////////////////
-//Ecouter la nombre de participation
-form.quantity.addEventListener('change',function() {
+// ////////////////////////////////////////////////////////
+// //ECOUTER LE NOMBRE DE PARTICIPATION
+// ////////////////////////////////////////////////////////
+form.quantity.addEventListener('input',function() {
   valideQuantity(this);
 });
-
 //Fonction vérifier que le nombre de participation est remplie
 const valideQuantity = function(inputQuantity) {
   let quantityRegExp = new RegExp(/^(\d)+$/,'g');
@@ -165,35 +215,27 @@ const valideQuantity = function(inputQuantity) {
   }};
 
 
+document.forms["reserve"];
 
-const locationCity1 = document.getElementById("location1");
-const locationCity2 = document.getElementById("location2");
-const locationCity3 = document.getElementById("location3");
-const locationCity4 = document.getElementById("location4");
-const locationCity5 = document.getElementById("location5");
-const locationCity6 = document.getElementById("location6");
-
-let valeurCity;
-if (document.getElementById('choix1').checked) {
- valeur = document.getElementById('choix1').value;
-}
-
-////////////////////////////////////////////////////////
-//Envoyer le formulaire
-form.addEventListener('submit', (event) => {
-
-  event.preventDefault();
-
-//EXEMPLE de regex Pour la DATE
-//let birthdateRegExp = new RegExp(/^(0?[1-9]|[12][0-9]|3[01])[\/](0?[1-9]|1[012])[\/]\d{4}$/, 'g')
-
-  console.log(form.first.value);
-  console.log(form.last.value);
-  console.log(form.email.value);
-  console.log(form.birthdate.value);
-  console.log(form.quantity.value);
+const locationCity = document.getElementsByName("location");
+let cityFormNodeList = document.querySelectorAll(".checkbox-input");
+let cityForm = Array.from(cityFormNodeList);
 
 
-  event.preventDefault();
-}
-);
+
+
+
+
+
+cityFormNodeList.addEventListener("input", function() {
+  validateCity(this);
+});
+
+const validateCity = function(inputCity) {
+let errorLocationCity = document.querySelector("#errorlocationcity");
+if (locationCity.value == "") {
+  errorLocationCity.innerHTML="Veuillez choisir une ville pour la participation au tournoi.";
+} else {
+    errorLocationCity.innerHTML="OK.";
+
+}};
